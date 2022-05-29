51bfd0a915eae78e226f866aeb790ec81a4b1e0c
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-05-29 15:39:46 +0900

    feat: first commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,29 @@
 import type { GatsbyConfig } from 'gatsby';
+const path = require('path');
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
 
 const config: GatsbyConfig = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-microcms',
+      options: {
+      apiKey: process.env.API_KEY,
+      serviceId: 'flyingcat',
+      apis: [
+        {
+          endpoint: 'works',
+          },
+        ],
+      },
+    }
+  ],
   jsxRuntime: `automatic`,
+  graphqlTypegen: true,
 };
 
 export default config;
