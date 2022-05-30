1844779f2e75fed7dea9df58b7cd2dafb95c9a99
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-05-31 00:14:53 +0900

    feat: add gatsby-source-figma

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -20,7 +20,39 @@ const config: GatsbyConfig = {
           },
         ],
       },
-    }
+    },
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    },
+    {
+      resolve: `gatsby-source-figma`,
+      options: {
+        // For files:
+        fileId: `FIGMA_FILE_ID`,
+        // For images:
+        nodeIds: [`FIGMA_NODE_IDS`],
+        // optional for nodeIds: A number between 0.01 and 4, the image scaling factor
+        scale: 1,
+        // optional: A string enum for the image output format, can be jpg, png, svg, or pdf
+        format: 'png',
+        // For projects:
+        projectId: `FIGMA_PROJECT_ID`,
+        // Get an access token from Figma Account Settings.
+        accessToken: `YOUR_FIGMA_ACCESS_TOKEN`,
+      },
+    },
   ],
   jsxRuntime: `automatic`,
   graphqlTypegen: true,
