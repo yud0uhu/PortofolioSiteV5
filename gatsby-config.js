6f13dc27868720b148ad0ed6951b46ecc8eb54cc
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-06-07 03:39:18 +0900

    fix: graphqlTypegenの指定のみ修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,3 @@
-const { register } = require('esbuild-register/dist/node')
-
-register({
-  target: 'node16'
-})
-
-module.exports = require('./gatsby-config.ts')
+module.exports = {
+  graphqlTypegen: true,
+}
