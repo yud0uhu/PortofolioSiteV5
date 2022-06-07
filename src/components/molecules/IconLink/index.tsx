b58530dab5a7d14637bf0dbc4b32b4e98ce84222
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-06-07 21:05:52 +0900

    fix: aboutの各種アイコンにリンク付け

--- src/components/molecules/IconLink/index.tsx
+++ src/components/molecules/IconLink/index.tsx
@@ -9,10 +9,30 @@ import React from 'react';
 export default function IconLink() {
   return (
     <>
-      <Icon iconImageUrl={GitHubIcon} marginLeft={4} />
-      <Icon iconImageUrl={TwitterIcon} marginLeft={4} />
-      <Icon iconImageUrl={HatenaBookmarkIcon} marginLeft={4} />
-      <Icon iconImageUrl={ZennIcon} marginLeft={4} />
+      <a href={'https://github.com/'} target="_blank" rel="noreferrer">
+        <Icon iconImageUrl={GitHubIcon} marginLeft={4} />
+      </a>
+      <a
+        href={'https://twitter.com/denham95173179'}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Icon iconImageUrl={TwitterIcon} marginLeft={4} />
+      </a>
+      <a
+        href={'https://hackteck.hatenablog.com/'}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Icon iconImageUrl={HatenaBookmarkIcon} marginLeft={4} />
+      </a>
+      <a
+        href={'https://zenn.dev/denham/scraps'}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Icon iconImageUrl={ZennIcon} marginLeft={4} />
+      </a>
     </>
   );
 }
