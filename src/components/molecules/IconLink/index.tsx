bbf8cbf93d067a864c28cc8a86725d23b719e114
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-06-11 11:22:11 +0900

    style: paddingを正常化

--- src/components/molecules/IconLink/index.tsx
+++ src/components/molecules/IconLink/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Spacer, Text } from '@chakra-ui/react';
 import { Icon } from '@/components/atoms/Icon/index';
-import HatenaBookmarkIcon from '@/static/cib_hatena-bookmark.png';
-import ZennIcon from '@/static/simple-icons_zenn.png';
+import { SiHatenabookmark } from '@react-icons/all-files/si/SiHatenabookmark';
+import { SiZendesk } from '@react-icons/all-files/si/SiZendesk';
+
 import React from 'react';
 import { FaGithubSquare } from '@react-icons/all-files/fa/FaGithubSquare';
 import { FaTwitterSquare } from '@react-icons/all-files/fa/FaTwitterSquare';
@@ -9,28 +10,32 @@ export default function IconLink() {
   return (
     <>
       <a href={'https://github.com/'} target="_blank" rel="noreferrer">
-        <FaGithubSquare size="100" color={'#40322A'} />
+        <FaGithubSquare size="lg" color={'#40322A'} />
       </a>
+      <Box px={2} />
       <a
         href={'https://twitter.com/denham95173179'}
         target="_blank"
         rel="noreferrer"
       >
-        <FaTwitterSquare size="100" color={'#40322A'} />
+        <FaTwitterSquare size="lg" color={'#40322A'} />
       </a>
+      <Box px={2} />
       <a
         href={'https://hackteck.hatenablog.com/'}
         target="_blank"
         rel="noreferrer"
       >
-        <Icon iconImageUrl={HatenaBookmarkIcon} marginLeft={4} />
+        <Box px={2} />
+        <SiHatenabookmark size="lg" color={'#40322A'} />
       </a>
       <a
         href={'https://zenn.dev/denham/scraps'}
         target="_blank"
         rel="noreferrer"
       >
-        <Icon iconImageUrl={ZennIcon} marginLeft={4} h={'48px'} w={'48px'} />
+        {/* <Box m={2}/>
+         <SiZendesk size="lg" color={'#40322A'} /> */}
       </a>
     </>
   );
