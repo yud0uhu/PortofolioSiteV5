e5976857db83a5517c6d64c000756d52c16fe24e
denham <kurokawashiori@kurokawashiorinoMacBook-Air.local>
2023-03-11 18:57:26 +0900

    fix: spでアイコンのmarginが崩れていたため修正

--- src/components/molecules/IconLink/index.tsx
+++ src/components/molecules/IconLink/index.tsx
@@ -8,12 +8,12 @@ const IconLink: React.FC = () => {
   return (
     <>
       <a href={'https://github.com/yud0uhu'} target="_blank" rel="noreferrer">
-        <Box mr={2} width={{ base: '49px', md: '108px' }}>
+        <Box mr={2} width={{ base: '32px', md: '108px' }}>
           <FaGithubSquare size="lg" color={'#40322A'} />
         </Box>
       </a>
       <a href={'https://github.com/yud0uhu'} target="_blank" rel="noreferrer">
-        <Box mr={2} width={{ base: '49px', md: '108px' }}>
+        <Box mr={2} width={{ base: '32px', md: '108px' }}>
           <FaTwitterSquare size="lg" color={'#40322A'} />
         </Box>
       </a>
@@ -22,12 +22,20 @@ const IconLink: React.FC = () => {
         rel="noreferrer"
         target="_blank"
       >
-        <Box mr={2} mt={2} width={{ base: '49px', md: '108px' }}>
+        <Box
+          mr={2}
+          mt={{ base: 0, sm: 2 }}
+          width={{ base: '32px', md: '108px' }}
+        >
           <SiHatenabookmark size="lg" color={'#40322A'} />
         </Box>
       </a>
       <a href={'https://zenn.dev/denham/'} rel="noreferrer" target="_blank">
-        <Box mr={2} mt={2} width={{ base: '49px', md: '108px' }}>
+        <Box
+          mr={2}
+          mt={{ base: 0, sm: 2 }}
+          width={{ base: '32px', md: '108px' }}
+        >
           <ZennSvgIcon color={'#40322A'} />
         </Box>
       </a>
